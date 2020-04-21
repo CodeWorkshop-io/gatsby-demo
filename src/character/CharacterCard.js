e37aa078f2536644658b88f6280020d51a3074b5
Chad Faurie <chad@codeworkshop.io>
2020-04-21 17:11:28 +0200

    Working Character Cards
    Co-authored-by: vivianelaure20 <vivianelauresop85@gmail.com>

--- src/character/CharacterCard.js
+++ src/character/CharacterCard.js
@@ -1,20 +1,110 @@
 import React from "react"
 
-import { Card, CardActions, Container, Typography } from "@material-ui/core"
+import {
+  makeStyles,
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+  Avatar,
+} from "@material-ui/core"
 
-const CharacterCard = ({ children }) => {
+import { red } from "@material-ui/core/colors"
+
+// how to style your components in Material
+const useStyles = makeStyles(theme => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}))
+
+// This is half correct
+// if you think about the props that are coming in here form the parent being the Characters.js File, you will see we spread the items from the array into this component one by one
+// effectively this receives, one characte, not a list
+
+// the children prop is only present if you pass something into the dom object eg <div>cake</div> will have a child of 'cake'
+
+// what you should do if you dont know what gets passed in here is just use props for starters
+// then log it to console
+
+const CharacterCard = props => {
+  // Use Styles hook from material, look above
+  const classes = useStyles()
+
+  console.log(props)
+
+  // or we can return is as json   eg
+  // return (
+  //   <div>
+  //     {JSON.stringify(props, null, 2)}
+  //     {/* that will pretty print it */}
+  //   </div>
+  // )
+
+  // as a starter
+  // return <Card></Card>
+
+  // copied from material ui https://material-ui.com/components/cards/#complex-interaction
   return (
-    <Card>
-      <Container maxWidth="sm">
-        <Typography
-          component="div"
-          style={{ backgroundColor: "#cfe8fc", height: "80vh" }}
-        >
-          List of Characters
-          <Typography>{children}</Typography>
+    <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            {props.name.substr(0, 1)}
+          </Avatar>
+        }
+        title={props.name}
+        subheader={props.created}
+      />
+      <CardMedia
+        className={classes.media}
+        image={props.image}
+        title={props.name}
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          Gender: {props.gender} <br />
+          Species: {props.species} <br />
+          Status: {props.status} <br />
+          {props.type ? (
+            <>
+              Type: {props.type} <br />
+            </>
+          ) : (
+            undefined
+          )}
+          Origin: {props.origin.name}
+          Location: {props.location.name}
         </Typography>
-      </Container>
-      <CardActions></CardActions>
+      </CardContent>
+      <CardActions disableSpacing>
+        {/* <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton> */}
+      </CardActions>
     </Card>
   )
 }
--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
 
+import { Grid } from "@material-ui/core"
+
 import Layout from "../components/layout"
 import CharacterCard from "../character/CharacterCard"
 
@@ -24,14 +26,20 @@ const Characters = () => {
     getCharacters()
   }, [])
 
+  // There are 2 options here, a GridList or a Grid of Cards
+  // https://material-ui.com/components/grid-list/
+  // https://material-ui.com/components/cards/
   return (
     <Layout>
       <br />
-      <>
-        {characters.map(i => (
-          <CharacterCard {...characters} />
+      <Grid container spacing={2}>
+        {characters.map((c, i) => (
+          <Grid key={i} item xs={3}>
+            {/* You want to spread the charater items from the map into the character card so that we can access the props from the api values */}
+            <CharacterCard {...c} />
+          </Grid>
         ))}
-      </>
+      </Grid>
     </Layout>
   )
 }
