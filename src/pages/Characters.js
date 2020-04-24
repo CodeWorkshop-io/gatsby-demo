76758ed5a83b25fbe0b7c5be7e10acd103dd4d80
vivianelaure20 <vivianelauresop85@gmail.com>
2020-04-24 08:03:26 +0200

    PaginationLocation

--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
-
 import { TableContainer, Table } from "@material-ui/core"
 
 import Layout from "../components/layout"
 import CharacterCard from "../character/CharacterCard"
 
-const Characters = () => {
+const Characters = props => {
   const [characters, setCharacters] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     async function getCharacters() {
       try {
+        setLoading(true)
         const response = await axios.get(
           "https://rickandmortyapi.com/api/character/"
         )
@@ -19,19 +20,21 @@ const Characters = () => {
         const result = response.data && response.data.results
 
         setCharacters(result)
+        setLoading(false)
       } catch (e) {
         console.error(e)
       }
     }
     getCharacters()
   }, [])
+  console.log(characters)
 
   return (
     <Layout>
       <br />
       <Table container spacing={2}>
         {characters.map((c, i) => (
-          <TableContainer key={i} item xs={6}>
+          <TableContainer key={i} item xs={4}>
             <CharacterCard {...c} />
           </TableContainer>
         ))}
--- src/pages/Locations.js
+++ src/pages/Locations.js
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
-import { Grid } from "@material-ui/core"
+import Button from "@material-ui/core/Button"
 import Layout from "../components/layout"
-import LocationCard from "../character/LocationCard"
 
 const Locations = () => {
   const [locations, setLocations] = useState()
+  const [loading, setLoading] = useState(false)
+
+  const [currentPage, setCurrentPage] = useState(1) //for beginning at the first page
+
+  const [locationPerPage, setLocationPerPage] = useState(10) // how many do you want per page
 
   useEffect(() => {
     async function getLocations() {
       try {
+        setLoading(true)
         const response = await axios.get(
           "https://rickandmortyapi.com/api/location/"
         )
@@ -17,26 +22,42 @@ const Locations = () => {
         const result = response.data && response.data.results
 
         setLocations(result)
+
+        setLoading(false)
       } catch (e) {
         console.error(e)
       }
     }
     getLocations()
   }, [])
+  console.log(locations)
+
+  //Get Current Locations
+  const indexOfLastLocation = currentPage * locationPerPage
+  const indexOfFirstLocation = indexOfLastLocation - locationPerPage
+  const currentLocation = locations.slice(
+    indexOfFirstLocation,
+    indexOfLastLocation
+  )
+  const IndexOfNextLocation = indexOfFirstLocation - locationPerPage
+  const NextLocation = locations.slice(
+    indexOfFirstLocation,
+    IndexOfNextLocation
+  )
 
   return (
     <Layout>
       <br />
 
-      <Grid container spacing={5}>
-        {/* For spacing between each grid */}
+      <Button variant="contained" onClick={currentLocation}>
+        Previous
+        <pre>{JSON.stringify(currentLocation, null, 2)}</pre>
+      </Button>
 
-        {locations.map((c, i) => (
-          <Grid key={i} item xs={4}>
-            <LocationCard {...c} />
-          </Grid>
-        ))}
-      </Grid>
+      <Button variant="contained" onClick={NextLocation}>
+        Next
+        <pre>{JSON.stringify(NextLocation, null, 2)}</pre>
+      </Button>
     </Layout>
   )
 }
